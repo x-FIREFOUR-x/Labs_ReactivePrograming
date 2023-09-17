f94adc640026719138ea4c2a13dd0aab3e11b379
Alex <olexandrpasalsky@gmail.com>
2023-09-17 19:27:55 +0300

    part2 task3

--- Lab1/Binding1/src/app/app.component.ts
+++ Lab1/Binding1/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: ` <input type="text" [value]="name" />
-                <input type="text" [value]="age" />
-                <p [textContent]="name"></p>`
+    template: ` <table border="1">
+                <tr><td [attr.colspan]="colspan">One-Two</td></tr>
+                <tr><td>Three</td><td>Four</td></tr>
+                <tr><td>Five</td><td>Six</td></tr>
+                </table>`
 })
 export class AppComponent {
     name = "Tom";
     age = 25;
+    colspan = 2;
 }
