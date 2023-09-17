f9ffefbce192aeed434c1288c017a7619dca9976
Alex <olexandrpasalsky@gmail.com>
2023-09-17 19:31:27 +0300

    part2 task4

--- Lab1/Binding1/src/app/app.component.ts
+++ Lab1/Binding1/src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: ` <table border="1">
-                <tr><td [attr.colspan]="colspan">One-Two</td></tr>
-                <tr><td>Three</td><td>Four</td></tr>
-                <tr><td>Five</td><td>Six</td></tr>
-                </table>`
+    template: `<p>Кількість кліків {{count}}</p>
+                <button (click)="increase()">Click</button>
+                <p>Кількість кліків {{count_2}}</p>
+                <button (click)="increase_2($event)">Click</button>`
 })
 export class AppComponent {
     name = "Tom";
     age = 25;
     colspan = 2;
+    count: number = 0;
+    count_2: number = 0;
+    increase() : void {
+        this.count++;
+    }
+
+    increase_2($event : any) : void {
+        this.count_2++;
+        console.log($event);
+    }
 }
