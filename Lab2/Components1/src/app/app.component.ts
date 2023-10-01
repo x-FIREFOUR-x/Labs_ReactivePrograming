924f2c7f4e3480a7e5b44794bfb7997506750322
Alex <olexandrpasalsky@gmail.com>
2023-10-01 14:19:28 +0300

    Component1 task1

---
+++ Lab2/Components1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: `<label>Введіть ім'я:</label>
+                <input [(ngModel)]="name" placeholder="name">
+                <h1>Ласкаво просимо {{name}}!</h1>`
+})
+export class AppComponent {
+    name = '';
+}
