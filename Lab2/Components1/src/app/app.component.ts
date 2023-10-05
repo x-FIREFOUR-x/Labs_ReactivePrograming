03ce3f27d2a20b48eef782135bae9302dfc03d0b
Alex <olexandrpasalsky@gmail.com>
2023-10-05 14:31:42 +0300

    Component1 task2

--- Lab2/Components1/src/app/app.component.ts
+++ Lab2/Components1/src/app/app.component.ts
@@ -3,7 +3,22 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `<label>Введіть ім'я:</label>
                 <input [(ngModel)]="name" placeholder="name">
-                <h1>Ласкаво просимо {{name}}!</h1>`
+                <h1>Ласкаво просимо {{name}}!</h1>
+                
+                <h2>Hello Angular</h2>
+                <p>Angular 16 представляє модульну архітектуру додатку</p>`,
+    styles: [` 
+                h2,h3{color:navy;}
+                p{font-size:13px; font-family:Verdana;}
+                
+                h1, h2{color:navy;}
+                p{font-size:13px;}
+                :host {
+                    font-family: Verdana;
+                    color: #555;
+                }
+            `]
+
 })
 export class AppComponent {
     name = '';
