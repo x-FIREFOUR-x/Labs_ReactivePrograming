4986e6dc1d0291fec0728fb13413beaaf8bd0ff1
Alex <olexandrpasalsky@gmail.com>
2023-10-05 20:11:33 +0300

    Component1 task6

--- Lab2/Components1/src/app/app.component.ts
+++ Lab2/Components1/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { 
     name = 'Петро';
-}
+    name2:string="Tom";
+    age:number = 24;
+   }
+   
--- Lab2/Components1/src/app/child.component.ts
+++ Lab2/Components1/src/app/child.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'child-comp',
-    template: `<ng-content></ng-content> 
-                <p>Привіт {{name}}</p>`,
+    template: ` <ng-content></ng-content>
+                <p>Привіт {{name}}</p>
+                <p>Ім’я користувача: {{userName}}</p>
+                <p>Вік користувача: {{userAge}}</p>`,
     styles: [`h2, p {color:red;}`]
 })
 export class ChildComponent {
     name = "Тарас";
+    @Input() userName: string = "";
+    @Input() userAge: number = 0;
 }
