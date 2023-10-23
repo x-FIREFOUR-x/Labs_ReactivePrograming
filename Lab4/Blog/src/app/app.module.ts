f364e12ea228f22a102ad44c6c81978ad242f50b
Alex <olexandrpasalsky@gmail.com>
2023-10-23 19:38:52 +0300

    task5-10

--- Lab4/Blog/src/app/app.module.ts
+++ Lab4/Blog/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
