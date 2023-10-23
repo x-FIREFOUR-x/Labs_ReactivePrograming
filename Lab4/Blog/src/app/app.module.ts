7a1dff10034a7aa7601510debc5fcfe5c8056e31
Alex <olexandrpasalsky@gmail.com>
2023-10-23 21:15:21 +0300

    task16-19

--- Lab4/Blog/src/app/app.module.ts
+++ Lab4/Blog/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
@@ -14,7 +14,8 @@ import { PostFormComponent } from './post-form/post-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Lab4/Blog/src/app/post-form/post-form.component.ts
+++ Lab4/Blog/src/app/post-form/post-form.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../app.component';
 
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
   styleUrls: ['./post-form.component.css']
 })
-export class PostFormComponent {
+export class PostFormComponent implements OnInit{
+  title='';
+  text='';
+  
+  constructor() {}
+  ngOnInit(): void {}
 
+  addPost(){
+    if (this.title.trim() && this.text.trim()){
+      const post: Post = {
+        title:this.title,
+        text:this.text
+      }
+      console.log('New post', post);
+      this.title = this.text = ''; // очищення полів 
+    }
+  }
 }
