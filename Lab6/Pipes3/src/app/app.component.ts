0e9450e5a8a05e72899fb537680ae5158c9b64aa
Alex <olexandrpasalsky@gmail.com>
2023-11-02 20:43:18 +0200

    Pipes3

---
+++ Lab6/Pipes3/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+import { Observable } from 'rxjs';
+
+@Component({
+    selector: 'my-app',
+    template: `<ul>
+                    <li *ngFor="let user of users | async">
+                    <p>Ім’я користувача: {{user.name}}</p>
+                    <p>Вік користувача: {{user.age}}</p>
+                    </li>
+                </ul>`,
+    providers: [HttpService]
+})
+export class AppComponent implements OnInit {
+
+    users: Observable<Object> | undefined;;
+    constructor(private httpService: HttpService) { }
+    ngOnInit() {
+        this.users = this.httpService.getUsers();
+        console.log(this.users)
+    }
+}
