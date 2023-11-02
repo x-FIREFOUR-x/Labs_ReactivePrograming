f81c75b93fe3ff60a50dfd59a4fb1a0db3918263
Alex <olexandrpasalsky@gmail.com>
2023-11-02 20:55:45 +0200

    Blog: integrate with Lab4

---
+++ Lab6/Blog/src/app/post-form/post-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Post } from '../app.component';
+
+@Component({
+  selector: 'app-post-form',
+  templateUrl: './post-form.component.html',
+  styleUrls: ['./post-form.component.css']
+})
+export class PostFormComponent implements OnInit{
+  @Output() onAdd:EventEmitter<Post> = new EventEmitter<Post>()
+  title='';
+  text='';
+  
+  constructor() {}
+  ngOnInit(): void {}
+
+  addPost(){
+    if (this.title.trim() && this.text.trim()){
+      const post: Post = {
+        title:this.title,
+        text:this.text
+      }
+      this.onAdd.emit(post);
+      console.log('New post', post);
+      this.title = this.text = ''; // очищення полів 
+    }
+  }
+}
