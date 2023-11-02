f1d3e86d4fee140a5d3543972185370d63456336
Alex <olexandrpasalsky@gmail.com>
2023-11-02 19:40:26 +0200

    Pipes2: Async pipes

--- Lab6/Pipes2/src/app/app.component.ts
+++ Lab6/Pipes2/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'my-app',
@@ -7,10 +9,20 @@ import { Component } from '@angular/core';
                 <hr/>
                 <input #user name="user" class="form-control">
                 <button class="btn" (click)="users.push(user.value)">Add</button>
-                <p>{{users | join}}</p>`
+                <p>{{users | join}}</p>
+                
+                <p>Модель: {{ phone| async }}</p>
+                <button (click)="showPhones()">Подивитися моделі</button>`
 })
 
 export class AppComponent {
     num: number = 15.45;
     users = ["Tom", "Alice", "Sam", "Kate", "Bob"];
+
+    phones = ["iPhone 7", "LG G 5", "Honor 9", "Idol S4", "Nexus 6P"];
+    phone: Observable<string> | undefined;
+    constructor() { this.showPhones(); }
+    showPhones() {
+        this.phone = interval(500).pipe(map((i: number) => this.phones[i]));
+    }
 }
