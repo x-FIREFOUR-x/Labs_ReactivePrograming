cda4edf3d07dd43295a3c39bb18b631b9080825e
Alex <olexandrpasalsky@gmail.com>
2023-11-02 18:23:17 +0200

    Pipes1: build in pipes

--- Lab6/Pipes1/src/app/app.component.ts
+++ Lab6/Pipes1/src/app/app.component.ts
@@ -3,9 +3,15 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `<div>Без форматування: {{myDate}}</div>
-                <div>З форматуванням: {{myDate | date}}</div>`
+                <div>З форматуванням: {{myDate | date}}</div>
+                <div>{{welcome | uppercase}}</div>
+                <div>{{welcome | lowercase}}</div>
+                <div>{{persentage | percent}}</div>
+                <div>{{persentage | currency}}</div>`
 })
 
 export class AppComponent {
     myDate = new Date(1961, 3, 12);
+    welcome: string = "Hello World!";
+    persentage: number = 0.14; 
 }
