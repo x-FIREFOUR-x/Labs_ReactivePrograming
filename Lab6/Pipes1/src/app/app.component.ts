81ea5a967aba5941a810d02d05279f69eb910814
Alex <olexandrpasalsky@gmail.com>
2023-11-02 18:36:01 +0200

    Pipes1: format money

--- Lab6/Pipes1/src/app/app.component.ts
+++ Lab6/Pipes1/src/app/app.component.ts
@@ -16,7 +16,13 @@ import { Component } from '@angular/core';
                 <div>{{myNewDate | date:"dd/MM/yyyy"}}</div>
                 
                 <div>{{pi | number:'2.1-2'}}</div>
-                <div>{{pi | number:'3.5-5'}}</div>`
+                <div>{{pi | number:'3.5-5'}}</div>
+                
+                <div>{{money | currency:'RUB':'code'}}</div>
+                <div>{{money | currency:'RUB':'symbol-narrow'}}</div>
+                <div>{{money | currency:'RUB':'symbol':'1.1-1'}}</div>
+                <div>{{money | currency:'RUB':'symbol-narrow':'1.1-1'}}</div>
+                <div>{{money | currency:'RUB':'тока седня по цене '}}</div>`
 })
 
 export class AppComponent {
@@ -25,4 +31,5 @@ export class AppComponent {
     persentage: number = 0.14;
     myNewDate = Date.now();
     pi: number = 3.1415;
+    money: number = 23.45;
 }
