8f3c3bc895f53f2f36377618de3249f4cbba925c
Alex <olexandrpasalsky@gmail.com>
2023-11-02 18:25:32 +0200

    Pipes1: format date

--- Lab6/Pipes1/src/app/app.component.ts
+++ Lab6/Pipes1/src/app/app.component.ts
@@ -9,11 +9,13 @@ import { Component } from '@angular/core';
                 <div>{{persentage | percent}}</div>
                 <div>{{persentage | currency}}</div>
                 <div>{{welcome | slice:3}}</div>
-                <div>{{welcome | slice:6:11}}</div>`
+                <div>{{welcome | slice:6:11}}</div>
+                <div>{{myNewDate | date:"dd/MM/yyyy"}}</div>`
 })
 
 export class AppComponent {
     myDate = new Date(1961, 3, 12);
     welcome: string = "Hello World!";
-    persentage: number = 0.14; 
+    persentage: number = 0.14;
+    myNewDate = Date.now();
 }
