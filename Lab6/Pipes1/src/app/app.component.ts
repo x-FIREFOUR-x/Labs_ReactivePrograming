89d728cd8ddac5eadb96df2935d4109800bfd4b1
Alex <olexandrpasalsky@gmail.com>
2023-11-02 18:29:05 +0200

    Pipes1: format numbers

--- Lab6/Pipes1/src/app/app.component.ts
+++ Lab6/Pipes1/src/app/app.component.ts
@@ -4,13 +4,19 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `<div>Без форматування: {{myDate}}</div>
                 <div>З форматуванням: {{myDate | date}}</div>
+                
                 <div>{{welcome | uppercase}}</div>
                 <div>{{welcome | lowercase}}</div>
                 <div>{{persentage | percent}}</div>
                 <div>{{persentage | currency}}</div>
+                
                 <div>{{welcome | slice:3}}</div>
                 <div>{{welcome | slice:6:11}}</div>
-                <div>{{myNewDate | date:"dd/MM/yyyy"}}</div>`
+                
+                <div>{{myNewDate | date:"dd/MM/yyyy"}}</div>
+                
+                <div>{{pi | number:'2.1-2'}}</div>
+                <div>{{pi | number:'3.5-5'}}</div>`
 })
 
 export class AppComponent {
@@ -18,4 +24,5 @@ export class AppComponent {
     welcome: string = "Hello World!";
     persentage: number = 0.14;
     myNewDate = Date.now();
+    pi: number = 3.1415;
 }
