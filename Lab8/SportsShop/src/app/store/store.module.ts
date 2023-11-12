0c1ac2c5505686400aa5573f2bc9da0c230993ac
Alex <olexandrpasalsky@gmail.com>
2023-11-13 00:31:11 +0200

    store

---
+++ Lab8/SportsShop/src/app/store/store.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { ModelModule } from "../model/model.module";
+import { StoreComponent } from "src/app/store/store.components";
+
+@NgModule({
+    imports: [ModelModule, BrowserModule, FormsModule],
+    declarations: [StoreComponent],
+    exports: [StoreComponent]
+})
+export class StoreModule { }
