b8bd60f4be3a7817720a93e4cca26bf0a18b5861
Alex <olexandrpasalsky@gmail.com>
2023-11-14 01:44:13 +0200

    update repository and datasource

--- Lab8/SportsShop/src/app/model/static.datasource.ts
+++ Lab8/SportsShop/src/app/model/static.datasource.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Product } from "./product.model";
 import { Observable, from } from "rxjs";
+import { Order } from "./order.model";
 
 @Injectable()
 export class StaticDataSource {
@@ -25,4 +26,10 @@ export class StaticDataSource {
     getProducts(): Observable<Product[]> {
         return from([this.products]);
     }
+
+    saveOrder(order: Order): Observable<Order> {
+        console.log(JSON.stringify(order));
+        return from([order]);
+    }
+        
 }
