bfcb7deb7faee6a2b48358d1d134e4bb7284a653
Alex <olexandrpasalsky@gmail.com>
2023-11-14 00:09:35 +0200

    route url

--- Lab8/SportsShop/src/app/store/store.module.ts
+++ Lab8/SportsShop/src/app/store/store.module.ts
@@ -5,11 +5,14 @@ import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.components";
 import { CounterDirective } from './counter.directive';
 import { CartSummaryComponent } from "./cartSummary.component";
+import { CartDetailComponent } from "./cartDetail.component";
+import { CheckoutComponent } from "./checkout.component";
+
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
-    exports: [StoreComponent]
+    declarations: [StoreComponent, CounterDirective, CartSummaryComponent, CartDetailComponent, CheckoutComponent],
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
 
 export class StoreModule { }
